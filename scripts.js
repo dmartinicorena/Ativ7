b8b357aafbdf9c0605b40553fd56b7dfe4d989fb
Diego Martinicorena <110708412+dmartinicorena@users.noreply.github.com>
2023-06-13 02:39:22 -0300

    fix news and add testimonials add news content and testimonials structure and functions

--- scripts.js
+++ scripts.js
@@ -1,16 +1,52 @@
+/*Countdown function for next IFSul Selection Process*/
+
+$(document).ready(function () {
+  // Countdown timer for next IFSul Selection Process
+  var targetDate = new Date("11/30/2023").getTime();
+  var countdownTimer = setInterval(function () {
+    var currentDate = new Date().getTime();
+    var distance = targetDate - currentDate;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    document.getElementById("countdown-days").innerHTML = days;
+
+    if (distance < 0) {
+      clearInterval(countdownTimer);
+      document.getElementById("countdown-days").innerHTML = "0";
+    }
+  }, 1000);
+});
+
+/* Carousel function for testimonials */
 $(document).ready(function() {
-    // Countdown timer for next IFSul Selection Process
-    var targetDate = new Date("11/30/2023").getTime();
-    var countdownTimer = setInterval(function() {
-        var currentDate = new Date().getTime();
-        var distance = targetDate - currentDate;
+    // Carousel configuration
+    var slideIndex = 0;
+    showSlides();
 
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        document.getElementById("countdown-days").innerHTML = days;
+    function showSlides() {
+        var slides = $(".testimonial-slide");
+        var dots = $(".carousel-nav button");
 
-        if (distance < 0) {
-            clearInterval(countdownTimer);
-            document.getElementById("countdown-days").innerHTML = "0";
+        // Hide all slides and remove active class from all dots
+        slides.hide();
+        dots.removeClass("active");
+
+        // Show the current slide and add active class to the current dot
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
         }
-    }, 1000);
+        slides.eq(slideIndex - 1).fadeIn();
+        dots.eq(slideIndex - 1).addClass("active");
+
+        // call showSlides() again after 6 seconds
+        setTimeout(showSlides, 6000);
+    }
+
+    // manually change slides
+    $(".carousel-nav button").click(function() {
+        var index = $(this).index();
+        slideIndex = index + 1;
+        showSlides();
+    });
 });
